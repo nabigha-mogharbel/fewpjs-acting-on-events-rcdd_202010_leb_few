f463dc6942e415bb5a17dd5c45d830c7ed33ff84
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-21 01:14:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let left = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
+  if (left > 179) {
     dodger.style.left = `${left - 1}px`;
   }
 }
