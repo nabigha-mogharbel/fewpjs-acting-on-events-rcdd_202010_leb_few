118df431b57b42cd213e3e7b4766e76f113c0aa3
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-21 01:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Your code here
 let dodger=document.getElementById('dodger');
 
-function moveDodgerLeft(){
+
   dodger.style.bachgroundColor='blue';
-}
 document.addEventListener("keydown", function(e) {
   console.log(e.key);
 })
