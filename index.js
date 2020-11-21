b451184959d317d7cebe6c50f2ddc43ccfbbf49f
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-21 01:17:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,21 +11,15 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
  
-  if (right > 0) {
-    dodger.style.left = `${right - 1}px`;
-  }
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;  }
 }
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
-  }
-});
-document.addEventListener("keydown", function(e) {
-  if (e.key === "Arrowright") {
-    moveDodgerRight();
   }
 });
